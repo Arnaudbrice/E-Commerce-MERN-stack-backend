5504e7a78112af068d4877f28c41a367abb3ea67
arnaudbrice <mephisteur@gmail.com>
2026-03-03 20:10:44 +0100

    add rate limit to chat

--- index.js
+++ index.js
@@ -81,7 +81,7 @@ const limiter = rateLimit({
 
 const chatLimiter = rateLimit({
   windowMs: 15 * 60 * 1000, // 15 minutes
-  max: 50, // limit each IP to 50 requests per windowMs
+  max: 100, // limit each IP to 50 requests per windowMs
   message: "Too many requests from this IP, please try again later.",
   standardHeaders: true,
   legacyHeaders: false,
