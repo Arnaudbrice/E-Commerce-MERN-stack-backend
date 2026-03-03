9bb46a0b70cee724ef1810aaaa875385a0eb98aa
arnaudbrice <mephisteur@gmail.com>
2026-03-03 20:17:49 +0100

    add rate limit to chatand to auth

--- index.js
+++ index.js
@@ -73,7 +73,7 @@ app.use(cors(corsOptions));
 // Apply rate limiting to all requests to prevent abuse and protect against brute-force attacks
 const limiter = rateLimit({
   windowMs: 15 * 60 * 1000, // 15 minutes
-  max: 100, // limit each IP to 100 requests per windowMs
+  max: 200, // limit each IP to 200 requests per windowMs
   message: "Too many requests from this IP, please try again later.",
   standardHeaders: true,
   legacyHeaders: false,
@@ -81,8 +81,15 @@ const limiter = rateLimit({
 
 const chatLimiter = rateLimit({
   windowMs: 15 * 60 * 1000, // 15 minutes
-  max: 100, // limit each IP to 50 requests per windowMs
+  max: 150, // limit each IP to 50 requests per windowMs
   message: "Too many requests from this IP, please try again later.",
+  standardHeaders: true,
+  legacyHeaders: false,
+});
+
+const authLimiter = rateLimit({
+  windowMs: 15 * 60 * 1000,
+  max: 50,
   standardHeaders: true,
   legacyHeaders: false,
 });
@@ -109,7 +116,7 @@ app.get("/health", async (req, res) => {
 
 //****** Routes specific middleware setting ******
 // public routes
-app.use("/auth", authRouter);
+app.use("/auth", authLimiter, authRouter);
 
 // protected routes
 // app.use(authenticate);
