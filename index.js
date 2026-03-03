14eaecc6d7b6f5bb3cc8e9462a72bd529dae1e8f
Brice Arnaud Habenicht <mephisteur@gmail.com>
2026-03-03 19:59:03 +0100

    Increase request limit to 200 per IP

--- index.js
+++ index.js
@@ -73,7 +73,7 @@ app.use(cors(corsOptions));
 // Apply rate limiting to all requests to prevent abuse and protect against brute-force attacks
 const limiter = rateLimit({
   windowMs: 15 * 60 * 1000, // 15 minutes
-  max: 100, // limit each IP to 100 requests per windowMs
+  max: 200, // limit each IP to 100 requests per windowMs
   message: "Too many requests from this IP, please try again later.",
   standardHeaders: true,
   legacyHeaders: false,
