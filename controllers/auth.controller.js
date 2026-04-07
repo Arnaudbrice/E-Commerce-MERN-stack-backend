c03198151bed557a166937ab459e6fffd08c378c
arnaudbrice <mephisteur@gmail.com>
2026-04-08 00:50:54 +0200

    feat: enhance logout functionality with partitioned cookies for improved privacy and security

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -114,6 +114,7 @@ export const logout = async (req, res) => {
     httpOnly: true,
     secure: process.env.NODE_ENV === "production",
     sameSite: process.env.NODE_ENV === "production" ? "none" : "lax",
+    partitioned: true, //help with modern browser privacy rules, ensures the cookie is treated as a partitioned cookie, which can help reduce certain types of cross-site request forgery (CSRF) attacks and enhance user privacy by isolating the cookie to the specific context of the site that set it.
   });
 
   res.status(204).json({ message: "Logged out successfully" });
